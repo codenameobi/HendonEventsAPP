2a25ef5b3699bf85ddd6c6e6d75f63cb2930abbe
admin <tobynwude@gmail.com>
2022-05-24 06:09:49 +0100

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace HendonEventsApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# HendonEventsAPP using MAUI
