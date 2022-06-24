c434e6e6644b7fbe4c712c15b82c5e961a1c727e
admin <tobynwude@gmail.com>
2022-06-24 23:39:18 +0100

    added MainViewModel

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace HendonEventsApp.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            Items = new ObservableCollection<string>();
+        }
+
+        [ObservableProperty]
+        ObservableCollection<string> items;
+
+        [ObservableProperty]
+        string text;
+
+        [ICommand]
+        public void Add()
+        {
+            if (string.IsNullOrWhiteSpace(Text))
+                return;
+            Items.Add(Text);
+            //add our item
+            Text = string.Empty;
+        }
+    }
+
+}
